85b375050d78b0b868b5307bc8993145375692cb
Ilja Melnicenko <melnicenko@wappler.systems>
2025-10-27 12:41:40 +0100

    inquiry

---
+++ Classes/Domain/Model/FormElements/ItemName.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace WapplerSystems\Inquiry\Domain\Model\FormElements;
+
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Form\Domain\Exception\IdentifierNotValidException;
+
+/**
+ * A generic form element
+ *
+ * Scope: frontend
+ */
+class ItemName extends \TYPO3\CMS\Form\Domain\Model\FormElements\GenericFormElement
+{
+
+
+}
