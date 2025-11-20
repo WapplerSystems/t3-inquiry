70090f6b03b9007a51678a4453de6f193b01a48c
Ilja Melnicenko <melnicenko@wappler.systems>
2025-11-20 13:24:10 +0100

    update BuildInquiryFormEvent

--- Classes/Event/BuildInquiryFormEvent.php
+++ Classes/Event/BuildInquiryFormEvent.php
@@ -2,16 +2,34 @@
 
 namespace WapplerSystems\Inquiry\Event;
 
+use TYPO3\CMS\Form\Domain\Model\FormElements\Section;
 use TYPO3\CMS\Form\Domain\Model\FormDefinition;
 
 class BuildInquiryFormEvent
 {
     private FormDefinition $formDefinition;
+    private Section $fieldsetProduct;
+    private string $hash;
 
-    public function __construct(FormDefinition $formDefinition)
+
+
+    public function __construct(FormDefinition $formDefinition, Section $fieldsetProduct, string $hash)
     {
         $this->formDefinition = $formDefinition;
+        $this->fieldsetProduct = $fieldsetProduct;
+        $this->hash = $hash;
     }
+
+    public function getFieldsetProduct(): Section
+    {
+        return $this->fieldsetProduct;
+    }
+
+    public function getHash(): string
+    {
+        return $this->hash;
+    }
+
 
     public function getFormDefinition(): FormDefinition
     {
